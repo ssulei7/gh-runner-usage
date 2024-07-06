b9c8bfc91390245e16b11233b98a6a72a8953716
Suleiman Suleiman <ssuleiman955@gmail.com>
2024-07-06 17:44:18 -0500

    chore: Update struct field types in actions.go

--- internal/actions/actions.go
+++ internal/actions/actions.go
@@ -8,23 +8,23 @@ import (
 type WorkflowRuns struct {
 	TotalCount   int `json:"total_count"`
 	WorkflowRuns []struct {
-		ID               int           `json:"id"`
-		Name             string        `json:"name"`
-		NodeID           string        `json:"node_id"`
-		CheckSuiteID     int           `json:"check_suite_id"`
-		CheckSuiteNodeID string        `json:"check_suite_node_id"`
-		HeadBranch       string        `json:"head_branch"`
-		HeadSha          string        `json:"head_sha"`
-		RunNumber        int           `json:"run_number"`
-		Event            string        `json:"event"`
-		Status           string        `json:"status"`
-		Conclusion       string        `json:"conclusion"`
-		WorkflowID       int           `json:"workflow_id"`
-		URL              string        `json:"url"`
-		HTMLURL          string        `json:"html_url"`
-		PullRequests     []interface{} `json:"pull_requests"`
-		CreatedAt        time.Time     `json:"created_at"`
-		UpdatedAt        time.Time     `json:"updated_at"`
+		ID               int       `json:"id"`
+		Name             string    `json:"name"`
+		NodeID           string    `json:"node_id"`
+		CheckSuiteID     int       `json:"check_suite_id"`
+		CheckSuiteNodeID string    `json:"check_suite_node_id"`
+		HeadBranch       string    `json:"head_branch"`
+		HeadSha          string    `json:"head_sha"`
+		RunNumber        int       `json:"run_number"`
+		Event            string    `json:"event"`
+		Status           string    `json:"status"`
+		Conclusion       string    `json:"conclusion"`
+		WorkflowID       int       `json:"workflow_id"`
+		URL              string    `json:"url"`
+		HTMLURL          string    `json:"html_url"`
+		PullRequests     []any     `json:"pull_requests"`
+		CreatedAt        time.Time `json:"created_at"`
+		UpdatedAt        time.Time `json:"updated_at"`
 		Actor            struct {
 			Login             string `json:"login"`
 			ID                int    `json:"id"`
@@ -183,46 +183,46 @@ type WorkflowRuns struct {
 				Type              string `json:"type"`
 				SiteAdmin         bool   `json:"site_admin"`
 			} `json:"owner"`
-			HTMLURL          string      `json:"html_url"`
-			Description      interface{} `json:"description"`
-			Fork             bool        `json:"fork"`
-			URL              string      `json:"url"`
-			ForksURL         string      `json:"forks_url"`
-			KeysURL          string      `json:"keys_url"`
-			CollaboratorsURL string      `json:"collaborators_url"`
-			TeamsURL         string      `json:"teams_url"`
-			HooksURL         string      `json:"hooks_url"`
-			IssueEventsURL   string      `json:"issue_events_url"`
-			EventsURL        string      `json:"events_url"`
-			AssigneesURL     string      `json:"assignees_url"`
-			BranchesURL      string      `json:"branches_url"`
-			TagsURL          string      `json:"tags_url"`
-			BlobsURL         string      `json:"blobs_url"`
-			GitTagsURL       string      `json:"git_tags_url"`
-			GitRefsURL       string      `json:"git_refs_url"`
-			TreesURL         string      `json:"trees_url"`
-			StatusesURL      string      `json:"statuses_url"`
-			LanguagesURL     string      `json:"languages_url"`
-			StargazersURL    string      `json:"stargazers_url"`
-			ContributorsURL  string      `json:"contributors_url"`
-			SubscribersURL   string      `json:"subscribers_url"`
-			SubscriptionURL  string      `json:"subscription_url"`
-			CommitsURL       string      `json:"commits_url"`
-			GitCommitsURL    string      `json:"git_commits_url"`
-			CommentsURL      string      `json:"comments_url"`
-			IssueCommentURL  string      `json:"issue_comment_url"`
-			ContentsURL      string      `json:"contents_url"`
-			CompareURL       string      `json:"compare_url"`
-			MergesURL        string      `json:"merges_url"`
-			ArchiveURL       string      `json:"archive_url"`
-			DownloadsURL     string      `json:"downloads_url"`
-			IssuesURL        string      `json:"issues_url"`
-			PullsURL         string      `json:"pulls_url"`
-			MilestonesURL    string      `json:"milestones_url"`
-			NotificationsURL string      `json:"notifications_url"`
-			LabelsURL        string      `json:"labels_url"`
-			ReleasesURL      string      `json:"releases_url"`
-			DeploymentsURL   string      `json:"deployments_url"`
+			HTMLURL          string `json:"html_url"`
+			Description      any    `json:"description"`
+			Fork             bool   `json:"fork"`
+			URL              string `json:"url"`
+			ForksURL         string `json:"forks_url"`
+			KeysURL          string `json:"keys_url"`
+			CollaboratorsURL string `json:"collaborators_url"`
+			TeamsURL         string `json:"teams_url"`
+			HooksURL         string `json:"hooks_url"`
+			IssueEventsURL   string `json:"issue_events_url"`
+			EventsURL        string `json:"events_url"`
+			AssigneesURL     string `json:"assignees_url"`
+			BranchesURL      string `json:"branches_url"`
+			TagsURL          string `json:"tags_url"`
+			BlobsURL         string `json:"blobs_url"`
+			GitTagsURL       string `json:"git_tags_url"`
+			GitRefsURL       string `json:"git_refs_url"`
+			TreesURL         string `json:"trees_url"`
+			StatusesURL      string `json:"statuses_url"`
+			LanguagesURL     string `json:"languages_url"`
+			StargazersURL    string `json:"stargazers_url"`
+			ContributorsURL  string `json:"contributors_url"`
+			SubscribersURL   string `json:"subscribers_url"`
+			SubscriptionURL  string `json:"subscription_url"`
+			CommitsURL       string `json:"commits_url"`
+			GitCommitsURL    string `json:"git_commits_url"`
+			CommentsURL      string `json:"comments_url"`
+			IssueCommentURL  string `json:"issue_comment_url"`
+			ContentsURL      string `json:"contents_url"`
+			CompareURL       string `json:"compare_url"`
+			MergesURL        string `json:"merges_url"`
+			ArchiveURL       string `json:"archive_url"`
+			DownloadsURL     string `json:"downloads_url"`
+			IssuesURL        string `json:"issues_url"`
+			PullsURL         string `json:"pulls_url"`
+			MilestonesURL    string `json:"milestones_url"`
+			NotificationsURL string `json:"notifications_url"`
+			LabelsURL        string `json:"labels_url"`
+			ReleasesURL      string `json:"releases_url"`
+			DeploymentsURL   string `json:"deployments_url"`
 		} `json:"head_repository"`
 	} `json:"workflow_runs"`
 }
